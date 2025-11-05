9dbe0be4f0bdfeb9f6c6d75bbd6007c04c0c59b5
gabrielchua <gabrielchuajd@gmail.com>
2025-11-06 02:09:25 +0800

    initial push

---
+++ website/config.js
@@ -0,0 +1,6 @@
+window.COFFEE_CONFIG = {
+  API_GW_BASE_URL_STR:
+    "https://v3xsaycjl6.execute-api.ap-southeast-1.amazonaws.com/prod",
+  COGNITO_LOGIN_BASE_URL_STR:
+    "https://ap-southeast-2qhan5bepz.auth.ap-southeast-2.amazoncognito.com/login?client_id=1mmb8ihaa5ltmarbj8kkg7orkg&response_type=token&scope=email+openid&redirect_uri=https%3A%2F%2Fd2r3aawh9ja091.cloudfront.net%2Fcallback.html",
+};
